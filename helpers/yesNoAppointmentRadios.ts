a26a22977c7995d9380577b621baebc2be193f62
Ana Bebic <57630381+LBHABEBIC@users.noreply.github.com>
2020-08-04 09:57:03 +0100

    feat: ITV-32 tweak question in repair (#15)

---
+++ helpers/yesNoAppointmentRadios.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    label: "Yes - appointment has been booked",
+    value: "yes booked",
+  },
+  {
+    label: "Yes - appointment has not been booked",
+    value: "not booked",
+  },
+  {
+    label: "No",
+    value: "no",
+  },
+];
